dc57663dbe68412b2c3c5861f36325f07c829dc0
Guilherme <gui20012006@gmail.com>
2025-10-08 12:43:59 -0300

    Nova funcionalidade

--- git_ex_cod1.cpp
+++ git_ex_cod1.cpp
@@ -5,6 +5,7 @@ int main (){
 
     cout << "Segundo commit, modificando para ver como o gitlog funciona" << endl;
     cout << "Nova modificação" << endl;
+    cout << "Segunda modificação " << endl;
 
     return 0;
 }
